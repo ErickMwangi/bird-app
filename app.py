628ec7482607bfe1d95e080e4c39ed88ccc616f3
ErickMwangi <rickthetri20@gmail.com>
2023-07-01 19:41:12 +0300

    initial commit

--- app.py
+++ app.py
@@ -6,18 +6,17 @@ from flask_restful import Api, Resource
 
 from models import db, Bird
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://erick_mwangi2:HEChbH3vMsoiYH2CuBXdUGRq02oXEY0g@dpg-cig2hftgkuvojjf3jg30-a.ohio-postgres.render.com/bird_app_lz1c'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+application = Flask(__name__)
+application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://erick_mwangi2:HEChbH3vMsoiYH2CuBXdUGRq02oXEY0g@dpg-cig2hftgkuvojjf3jg30-a.ohio-postgres.render.com/bird_app_lz1c'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+application.json.compact = False
 
-migrate = Migrate(app, db)
-db.init_app(app)
+migrate = Migrate(application, db)
+db.init_app(application)
 
-api = Api(app)
+api = Api(application)
 
 class Birds(Resource):
-
     def get(self):
         birds = [bird.to_dict() for bird in Bird.query.all()]
         return make_response(jsonify(birds), 200)
